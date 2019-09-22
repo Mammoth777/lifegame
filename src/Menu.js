3a82dd5b2f8a27467d207b22513cf91eefc699f3
hjq <154654738@qq.com>
2019-09-22 18:29:09 +0800

    天呐，都不记得啥时候写的这玩意了。。。

--- src/Menu.js
+++ src/Menu.js
@@ -11,6 +11,11 @@ class Menu extends Component{
         <button onClick={this.props.prevHandler}>prev</button>
         <button onClick={this.props.stopHandler}>stop</button>
         <button onClick={this.props.nextHandler}>next</button>
+        <ol>
+          <li>如果一个细胞周围有3个细胞为生（一个细胞周围共有8个细胞），则该细胞为生（即该细胞若原先为死，则转为生，若原先为生，则保持不变） 。</li>
+          <li>如果一个细胞周围有2个细胞为生，则该细胞的生死状态保持不变；</li>
+          <li>在其它情况下，该细胞为死（即该细胞若原先为生，则转为死，若原先为死，则保持不变）</li>
+        </ol>
       </div>
     )
   }
